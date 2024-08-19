14fbd2731682075f9163ecd130fdc305496a0ec2
AlexandreUff <alexandre.magno.wrk@gmail.com>
2024-08-19 18:36:43 -0300

    feat: redering the data request

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,21 +1,34 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
+  <q-page class="row items-center justify-evenly bg-red-600">
+    <!-- <example-component
       title="Example component"
       active
       :todos="todos"
       :meta="meta"
-    ></example-component>
+    ></example-component> -->
+
+    <ul class="row items-center justify-evenly bg-blue-600 pt-4">
+      <li
+        v-for="fixedGIF in fixedGIFs"
+        :key="fixedGIF.id"
+        class="py-2 px-1 shadow-2xl"
+      >
+        <!-- {{ fixedGIF.image }} -->
+        <q-img :src="fixedGIF.image" style="width: 200px; height: 200px" />
+      </li>
+    </ul>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
+import { /* ref, */ onMounted, Ref } from 'vue';
+// import { Todo, Meta } from 'components/models';
+// import ExampleComponent from 'components/ExampleComponent.vue';
 import APIService from 'src/services/APIService';
 
 const API = new APIService();
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let fixedGIFs: Ref<any>;
 
 onMounted(async () => {
   try {
@@ -23,6 +36,19 @@ onMounted(async () => {
       '/v1/gifs/trending?api_key=Ibyf64xoQQo6sUoTg2QbxMQI4MG5zrR4&limit=25&offset=0&rating=g&bundle=messaging_non_clips'
     );
     console.log(response.data);
+
+    fixedGIFs =
+      /* ref( */
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      response.data.data.map((gifsData: any) => {
+        return {
+          id: gifsData.id,
+          image: gifsData.images.fixed_height.url,
+        };
+      });
+    /* ); */
+
+    console.log('GIFs:', fixedGIFs);
   } catch (error) {
     console.error(error);
   }
@@ -32,7 +58,7 @@ defineOptions({
   name: 'IndexPage',
 });
 
-const todos = ref<Todo[]>([
+/* const todos = ref<Todo[]>([
   {
     id: 1,
     content: 'ct1',
@@ -57,5 +83,5 @@ const todos = ref<Todo[]>([
 
 const meta = ref<Meta>({
   totalCount: 1200,
-});
+}); */
 </script>
