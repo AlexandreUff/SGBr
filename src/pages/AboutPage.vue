bc7222fe9f6709955a8d008e63dcba0687d1bfba
AlexandreUff <alexandre.magno.wrk@gmail.com>
2024-08-20 17:57:56 -0300

    feat: create own about

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,3 +1,47 @@
-<template>SROBE</template>
+<template>
+  <q-page class="row items-center justify-evenly bg-[#451952] pt-8">
+    <q-layout class="max-w-5xl px-2">
+      <h2 class="text-lg text-[#F39F5A] font-bold">SOBRE MIM</h2>
+
+      <div class="flex align-middle justify-center mb-6">
+        <q-img src="~assets/img-dev.png" width="250px" class="rounded-full" />
+      </div>
+
+      <q-p class="block text-slate-300 pb-8 text-xl">
+        Olá! Eu me chamo
+        <q-span class="text-[#F39F5A]">Alexandre Magno</q-span>, desenvolvedor
+        full-stack com mais de 5 anos de experiência e formado em Tecnologia da
+        Informação pela Universidade Federal Fluminense (UFF).
+      </q-p>
+      <q-p class="block text-slate-300 pb-8 text-xl">
+        Este teste tem como finalidade apresentar meus conhecimentos com as
+        tecnologias requiridas visando garantir o máximo de qualidade possível
+        com as boas práticas e padrões do mundo do desenvolvimento.
+      </q-p>
+      <q-p class="block text-slate-300 pb-8 text-xl">
+        Ressalto que este é o 2º projeto na vida que crio usando Vue.Js e a
+        primeira vez com o Quasar o qual, até este momento, nunca tinha sequer
+        ouvido falar. Para além disso, eu tive um prazo ainda menor que o
+        requerido em virtude de compromissos inadiáveis neste último final de
+        semana. Iniciei no dia de ontem, 19/08, segunda-feira.
+      </q-p>
+      <q-p class="block text-slate-300 pb-8 text-xl">
+        Não é o tipo de coisa que eu costumo expor, mas espero que considere
+        este curto perído de tempo o qual tive disponível para entregar este
+        resultado final.
+      </q-p>
+      <q-p class="block text-slate-300 pb-8 text-xl">
+        Caso queira me conhecer mais, acesse meu site de apresentação:
+      </q-p>
+      <q-p class="block text-[#AE445A] pb-8 text-xl">
+        <a
+          href="https://alexandre-magno-apresentacao.netlify.app"
+          target="_blank"
+          >alexandre-magno-apresentacao.netlify.app</a
+        >
+      </q-p>
+    </q-layout>
+  </q-page>
+</template>
 
 <script setup lang="ts"></script>
