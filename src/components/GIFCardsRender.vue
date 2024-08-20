2e6cb538309246fe4e223bd03a6af2a502c5e2f2
AlexandreUff <alexandre.magno.wrk@gmail.com>
2024-08-20 10:04:59 -0300

    feat: create GIFCardsRender component and use it

---
+++ src/components/GIFCardsRender.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page class="row items-center justify-evenly bg-red-600">
+    <ul class="row items-center justify-evenly bg-blue-600 pt-4">
+      <li
+        v-for="fixedGIF in fixedGIFs"
+        :key="fixedGIF.id"
+        class="py-2 px-1 shadow-2xl relative"
+      >
+        <QCard
+          class="flex absolute top-3 right-2 z-10 bg-neutral-50 opacity-30 rounded-2xl p-1 cursor-pointer"
+          title="Adicionar aos favoritos"
+        >
+          <q-icon name="star" />
+        </QCard>
+        <q-img :src="fixedGIF.image" style="width: 200px; height: 200px" />
+      </li>
+    </ul>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+interface FixedGIF {
+  id: string;
+  image: string;
+}
+
+const { fixedGIFs } = defineProps<{ fixedGIFs: FixedGIF[] }>();
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,26 +1,13 @@
 <template>
   <q-page class="row items-center justify-evenly bg-red-600">
-    <ul class="row items-center justify-evenly bg-blue-600 pt-4">
-      <li
-        v-for="fixedGIF in fixedGIFs"
-        :key="fixedGIF.id"
-        class="py-2 px-1 shadow-2xl relative"
-      >
-        <QCard
-          class="flex absolute top-3 right-2 z-10 bg-neutral-50 opacity-30 rounded-2xl p-1 cursor-pointer"
-          title="Adicionar aos favoritos"
-        >
-          <q-icon name="star" />
-        </QCard>
-        <q-img :src="fixedGIF.image" style="width: 200px; height: 200px" />
-      </li>
-    </ul>
+    <GIFCardsRender :fixedGIFs="fixedGIFs" />
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 import APIService from 'src/services/APIService';
+import GIFCardsRender from 'src/components/GIFCardsRender.vue';
 
 const API = new APIService();
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
