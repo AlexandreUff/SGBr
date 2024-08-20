b1aba21df1cdb058944e0ed930a27efaedcf7345
AlexandreUff <alexandre.magno.wrk@gmail.com>
2024-08-20 16:00:32 -0300

    feat: add search input bar

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,19 @@
 <template>
   <q-page class="row items-center justify-evenly bg-[#451952] pt-8">
     <q-layout class="max-w-5xl px-2">
-      <h2 class="text-lg text-[#F39F5A] font-bold">PRINCIPAIS GIFs</h2>
+      <!-- <h2 class="text-lg text-[#F39F5A] font-bold">PRINCIPAIS GIFs</h2> -->
+      <div class="flex">
+        <q-input
+          v-model="searchText"
+          label="Digite o nome de algum GIF que queira pesquisar"
+          placeholder="Ex: Animais, aniversário, carros..."
+          class="flex-grow bg-[#ffffff80] px-2"
+          label-color="purple"
+        />
+        <QCard class="flex align-middle justify-center bg-[#AE445A] w-14 pt-2">
+          <q-icon name="search" size="lg" />
+        </QCard>
+      </div>
       <GIFCardsRender :GIFsDatas="fixedGIFs" />
     </q-layout>
   </q-page>
@@ -15,6 +27,7 @@ import GIFCardsRender from 'src/components/GIFCardsRender.vue';
 const API = new APIService();
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const fixedGIFs = ref<{ id: string; image: string }[]>([]);
+const searchText = ref<string>('');
 
 onMounted(async () => {
   try {
@@ -37,6 +50,31 @@ onMounted(async () => {
     console.error(error);
   }
 });
+
+// const updateCategorySelected = async (newCategoryName: string) => {
+//   categorySelected.value = newCategoryName;
+//   console.log(categorySelected.value);
+
+//   try {
+//     const response = await API.get(
+//       `/v1/gifs/search?api_key=Ibyf64xoQQo6sUoTg2QbxMQI4MG5zrR4&q=${newCategoryName}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+//     );
+//     console.log(response.data);
+
+//     fixedGIFs.value =
+//       // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//       response.data.data.map((gifsData: any) => {
+//         return {
+//           id: gifsData.id,
+//           image: gifsData.images.fixed_height.url,
+//         };
+//       });
+
+//     console.log('GIFs:', fixedGIFs);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
 
 defineOptions({
   name: 'IndexPage',
