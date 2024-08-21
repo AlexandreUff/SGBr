2994272f3dfaf262a8fd63a2f4f7f8f5f6072c6a
AlexandreUff <alexandre.magno.wrk@gmail.com>
2024-08-20 22:13:57 -0300

    feat: add fav method

--- src/components/GIFCardsRender.vue
+++ src/components/GIFCardsRender.vue
@@ -9,9 +9,11 @@
         <QCard
           class="flex absolute top-3 right-2 z-10 bg-neutral-50 opacity-30 rounded-2xl p-1 cursor-pointer"
           title="Adicionar aos favoritos"
+          @click="() => setFavorite(fixedGIF.id)"
         >
           <q-icon name="star" />
         </QCard>
+        <FavoriteButton GIFId="777" />
         <q-img :src="fixedGIF.image" style="width: 200px; height: 200px" />
       </li>
     </ul>
@@ -19,10 +21,20 @@
 </template>
 
 <script setup lang="ts">
+import { LocalStorage } from 'quasar';
+import { ref } from 'vue';
+
 interface GIFProps {
   id: string;
   image: string;
 }
 
 const { GIFsDatas } = defineProps<{ GIFsDatas: GIFProps[] }>();
+const favoritesList = ref<string[]>([]);
+
+const setFavorite = (GIFId: string) => {
+  favoritesList.value.push(GIFId);
+  console.log(favoritesList.value);
+  LocalStorage.set('meuArray', favoritesList.value);
+};
 </script>
